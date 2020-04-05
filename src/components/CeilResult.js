4c59a1d256c5123e50ff070bc762919a157a53cd
a-pompom <aoi.matsuda.3x@gmail.com>
2020-04-05 13:58:53 +0900

    計算結果表示ロジックの作成
    入力をもとに計算結果を表示可能とした。
    
    #3

--- src/components/CeilResult.js
+++ src/components/CeilResult.js
@@ -1,30 +1,58 @@
 import React from 'react'
 
-const ceilResult = (props) => (
+const JEWELRY_RATE = {'ticket': 300, 'ticket10': 3000}
+const TICKET_RATE = {'jewelry': 1/300, 'ticket10': 10}
 
-    <div className="CeilResult">
-        <img src={`${process.env.PUBLIC_URL}/Lyria.png`}  className="CeilResult__image" alt="ルリア" />
+const CEIL_JEWELRY = 90000
+const CEIL_TICKET = 300
 
-        <div className="CeilResult__Rest">
-            <div className="Rest__crystal">
-                <img src={`${process.env.PUBLIC_URL}/Crystal.png`}  width="100" alt="宝晶石" />
-                <h4>900</h4>
-                <h4>&nbsp;/&nbsp;</h4>
-                <h4>90000</h4>
-            </div>
-            <div className="Rest__ticket">
-                <img src={`${process.env.PUBLIC_URL}/1.png`}  width="100" alt="ガチャチケット" />
-                <h4>10</h4>
-                <h4>&nbsp;/&nbsp;</h4>
-                <h4>300</h4>
-            </div>
-            <div className="Rest__percentage">
-                <h4>10%</h4>
-                <h4>&nbsp;/&nbsp;</h4>
-                <h4>100%</h4>
+const CeilResult = ({ceilSaving, ceilReached}) => {
+
+    const jewelryAmount =  ceilSaving.jewelry
+    const ticketAmount =   ceilSaving.ticket
+    const ticket10Amount = ceilSaving.ticket10
+
+    const jewelrySum = jewelryAmount + (ticketAmount * JEWELRY_RATE['ticket']) + (ticket10Amount * JEWELRY_RATE['ticket10'])
+    const ticketSum = Math.floor((jewelryAmount * TICKET_RATE['jewelry']) + ticketAmount + (ticket10Amount * TICKET_RATE['ticket10']))
+
+    let progressRate = ((jewelrySum / CEIL_JEWELRY) * 100).toFixed(2)
+    progressRate = progressRate >= 100 ? 100 : progressRate
+
+    if (progressRate === 100) {
+        ceilReached()
+    }
+
+    return (
+
+        <div className="CeilResult">
+            <img src={`${process.env.PUBLIC_URL}/Lyria.png`}  className="CeilResult__image" alt="ルリア" />
+
+            <div className="CeilResult__Rest">
+
+                <div className="Rest__crystal">
+                    <img src={`${process.env.PUBLIC_URL}/Crystal.png`}  width="100" alt="宝晶石" />
+                    <h4>{jewelrySum}</h4>
+                    <h4>&nbsp;/&nbsp;</h4>
+                    <h4>{CEIL_JEWELRY}</h4>
+                </div>
+
+                <div className="Rest__ticket">
+                    <img src={`${process.env.PUBLIC_URL}/1.png`}  width="100" alt="ガチャチケット" />
+                    <h4>{ticketSum}</h4>
+                    <h4>&nbsp;/&nbsp;</h4>
+                    <h4>{CEIL_TICKET}</h4>
+                </div>
+
+                <div className="Rest__percentage">
+                    <h4>{progressRate}%</h4>
+                    <h4>&nbsp;/&nbsp;</h4>
+                    <h4>100%</h4>
+                </div>
+
             </div>
         </div>
-    </div>
-)
 
-export default ceilResult
+    )
+}
+
+export default CeilResult
